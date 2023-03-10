c7443efe10dfc3823a7f7219d5502a249cd0f3b1
Joe Vandzura <vandzura.joe.e@gmail.com>
2023-03-10 16:06:31 -0600

    refactored more code

--- src/main/java/servlets/LoginServlet.java
+++ src/main/java/servlets/LoginServlet.java
@@ -1,5 +1,6 @@
 package servlets;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -8,12 +9,19 @@ import java.io.IOException;
 
 @WebServlet(name = "loginServlet", urlPatterns = "/login")
 public class LoginServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        request.getRequestDispatcher("/login.jsp").forward(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
 
         if (username != null && username.equalsIgnoreCase("admin") && password != null && password.equalsIgnoreCase("password")) {
             response.sendRedirect("/profile.jsp");
+            return;
         }
+        request.getRequestDispatcher("/login.jsp").forward(request, response);
     }
+
 }
--- src/main/webapp/login.jsp
+++ src/main/webapp/login.jsp
@@ -1,15 +1,6 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 
-<%
-    String username = request.getParameter("username");
-    String password = request.getParameter("password");
-
-    if (username != null && username.equalsIgnoreCase("admin") && password != null && password.equalsIgnoreCase("password")) {
-        response.sendRedirect("/profile.jsp");
-    }
-%>
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -20,7 +11,7 @@
 <%@ include file="partials/navbar.jsp" %>
 <h1>Hello</h1>
 
-<form method="POST" action="login.jsp">
+<form method="POST" action="/login">
     <label for="username-input">Username</label>
     <input id="username-input" name="username" placeholder="Enter your username">
     <br>
